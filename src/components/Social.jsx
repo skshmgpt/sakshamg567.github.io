3737cd904d1c1dd22a3f36d58933cf969a4b7119
sakshamg567 <saksham060306@gmail.com>
2025-11-19 19:03:22 +0530

    minor changes

--- src/components/Social.jsx
+++ src/components/Social.jsx
@@ -1,9 +1,9 @@
 export default function Social({ Icon, Text, Link }) {
    return (
       <a href={Link} target="_blank" rel="noopener noreferrer">
-         <span className='items-center flex flex-row hover:text-white gap-1 transition-all duration-100'>
+         <span className='items-center flex flex-row hover:text-white gap-1.5 transition-all duration-100 justify-center text-center'>
             <Icon fontSize='small' />
-            {Text}
+            <span className='mt-0.5'>{Text}</span>
          </span>
       </a>
    )
